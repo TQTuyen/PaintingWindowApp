223033dc64e2109ca1d6af610ce620865801b73e
22120415 <tranquangtuyen135305@gmail.com>
2025-12-05 21:53:49 +0700

    feat: configure Mica backdrop for main window

--- PaintingApp/MainWindow.xaml.cs
+++ PaintingApp/MainWindow.xaml.cs
@@ -1,68 +1,58 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Composition.SystemBackdrops;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 using PaintingApp.Contracts;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace PaintingApp;
 
-namespace PaintingApp
+public sealed partial class MainWindow : Window
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
+    private readonly INavigationService _navigationService;
+
+    public MainWindow()
     {
-        private readonly INavigationService _navigationService;
+        InitializeComponent();
 
-        public MainWindow()
+        ExtendsContentIntoTitleBar = true;
+        SetTitleBar(AppTitleBar);
+        SetupMicaBackdrop();
+
+        _navigationService = App.GetService<INavigationService>();
+        _navigationService.Frame = ContentFrame;
+
+        ContentFrame.Navigated += OnFrameNavigated;
+    }
+
+    private void SetupMicaBackdrop()
+    {
+        if (MicaController.IsSupported())
         {
-            InitializeComponent();
+            SystemBackdrop = new MicaBackdrop { Kind = MicaKind.Base };
+        }
+    }
 
-            ExtendsContentIntoTitleBar = true;
-            SetTitleBar(AppTitleBar);
+    private void OnFrameNavigated(object sender, NavigationEventArgs e)
+    {
+        BackButton.IsEnabled = _navigationService.CanGoBack;
+    }
 
-            _navigationService = App.GetService<INavigationService>();
-            _navigationService.Frame = ContentFrame;
+    private void BackButton_Click(object sender, RoutedEventArgs e)
+    {
+        _navigationService.GoBack();
+    }
 
-            ContentFrame.Navigated += OnFrameNavigated;
+    private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+    {
+        if (args.IsSettingsSelected)
+        {
+            return;
         }
 
-        private void OnFrameNavigated(object sender, NavigationEventArgs e)
+        if (args.SelectedItemContainer is NavigationViewItem selectedItem)
         {
-            BackButton.IsEnabled = _navigationService.CanGoBack;
-        }
-
-        private void BackButton_Click(object sender, RoutedEventArgs e)
-        {
-            _navigationService.GoBack();
-        }
-
-        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
-        {
-            if (args.IsSettingsSelected)
-            {
-                return;
-            }
-
-            if (args.SelectedItemContainer is NavigationViewItem selectedItem)
-            {
-                var tag = selectedItem.Tag?.ToString();
-            }
+            var tag = selectedItem.Tag?.ToString();
         }
     }
 }
