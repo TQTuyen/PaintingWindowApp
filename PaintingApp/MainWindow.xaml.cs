a386ecd2cff5bc268b35a373cffcb7b1a900401b
22120415 <tranquangtuyen135305@gmail.com>
2025-12-05 20:09:42 +0700

    feat: implement custom TitleBar with back button

--- PaintingApp/MainWindow.xaml.cs
+++ PaintingApp/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+using PaintingApp.Contracts;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,9 +27,42 @@ namespace PaintingApp
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private readonly INavigationService _navigationService;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(AppTitleBar);
+
+            _navigationService = App.GetService<INavigationService>();
+            _navigationService.Frame = ContentFrame;
+
+            ContentFrame.Navigated += OnFrameNavigated;
+        }
+
+        private void OnFrameNavigated(object sender, NavigationEventArgs e)
+        {
+            BackButton.IsEnabled = _navigationService.CanGoBack;
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            _navigationService.GoBack();
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                return;
+            }
+
+            if (args.SelectedItemContainer is NavigationViewItem selectedItem)
+            {
+                var tag = selectedItem.Tag?.ToString();
+            }
         }
     }
 }
